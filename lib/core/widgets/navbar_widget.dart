9bf6e161039af60a1fa97f6d58305cf817083931
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-06-01 23:02:45 +0700

    feat: Add NavbarWidget with navigation functionality using Riverpod

---
+++ lib/core/widgets/navbar_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:test_app/core/providers/navigation_provider.dart';
+
+class NavbarWidget extends ConsumerWidget {
+  const NavbarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedPage = ref.watch(selectedPageProvider);
+    return NavigationBar(
+      backgroundColor: Colors.tealAccent,
+      indicatorColor: Colors.white,
+      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+      destinations: const [
+        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+        NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+        NavigationDestination(icon: Icon(Icons.touch_app), label: 'Gestures'),
+        NavigationDestination(icon: Icon(Icons.smart_button), label: 'Buttons'),
+      ],
+      selectedIndex: selectedPage,
+      onDestinationSelected: (int value) {
+        ref.read(selectedPageProvider.notifier).state = value;
+      },
+    );
+  }
+}
